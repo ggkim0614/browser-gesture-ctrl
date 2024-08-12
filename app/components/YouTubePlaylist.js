8921c311a17426fee573efbaadcd06e89187f6e7
George Kim <ggkim0614@gmail.com>
2024-08-11 18:08:39 -0700

    Add clicked state

--- app/components/YouTubePlaylist.js
+++ app/components/YouTubePlaylist.js
@@ -39,17 +39,19 @@ const YouTubePlaylist = ({ onVideoSelect }) => {
 					onVideoSelect(videoId);
 				}
 				setSelectedItem(null);
-			}, 1000)
+			}, 2000)
 		);
 	};
 
 	return (
 		<div
-			className="youtube-playlist clickable font-jbm grid gap-6 overflow-y-auto columns-1 sm:columns-2 lg:columns-4"
+			className="youtube-playlist clickable font-jbm overflow-y-auto overflow-x-hidden
+                 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"
 			style={{
 				padding: '20px',
 				boxSizing: 'border-box',
 				maxHeight: '100%',
+				width: '100%', // Ensure the container takes full width
 			}}
 			id="youtube-playlist"
 		>
@@ -57,17 +59,18 @@ const YouTubePlaylist = ({ onVideoSelect }) => {
 				<div
 					key={item.id}
 					onClick={() => handleItemSelect(item.snippet.resourceId.videoId)}
-					className={`cursor-pointer rounded-md border border-1 border-gray-200 shadow-md youtube-playlist-item clickable bg-white hover:bg-blue-100 ${
-						selectedItem === item.snippet.resourceId.videoId
-							? 'border-4 border-blue-500'
-							: ''
-					}`}
+					className={`cursor-pointer rounded-md shadow-md 
+                      youtube-playlist-item clickable bg-white hover:bg-blue-100 
+                      relative overflow-hidden`}
 					data-video-id={item.snippet.resourceId.videoId}
 					style={{
-						overflow: 'hidden',
 						display: 'flex',
 						flexDirection: 'column',
 						transition: 'all 0.3s ease',
+						border:
+							selectedItem === item.snippet.resourceId.videoId
+								? '4px solid #3B82F6'
+								: '1px solid #E5E7EB',
 					}}
 				>
 					<img
@@ -79,6 +82,16 @@ const YouTubePlaylist = ({ onVideoSelect }) => {
 					<div className="clickable text-2xl font-semibold p-4 justify-center items-center flex flex-grow">
 						{item.snippet.title}
 					</div>
+					{selectedItem === item.snippet.resourceId.videoId && (
+						<div
+							className="z-50 absolute inset-0 bg-blue-500 bg-opacity-50 flex items-center justify-center text-center"
+							style={{ pointerEvents: 'none' }}
+						>
+							<span className="text-white text-xl font-bold">
+								Click again to start playing
+							</span>
+						</div>
+					)}
 				</div>
 			))}
 		</div>
